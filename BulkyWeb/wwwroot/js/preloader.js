af9b6a5a68f2f7dc6ef806d438ee0fabb1fb3552
udoy2 <udoyrahman983@gmail.com>
2024-05-15 22:37:06 +0600

    added preloader and added screenshots

---
+++ BulkyWeb/wwwroot/js/preloader.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('load', function () {
+        var preloader = document.querySelector('.loader_container');
+        preloader.style.display = 'none';
+    });
+});
